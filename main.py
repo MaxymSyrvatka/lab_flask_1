d7c1e4db82dfd7968c253d2883175bca09c5aabe
MaxymSyrvatka <maximizes258@gmail.com>
2020-11-03 22:59:47 +0200

    first commit

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from wsgiref.simple_server import make_server
 app = Flask(__name__)
 
 
-@app.route('/api/v1/hello-world-27')
+@app.route('/api/v1/hello-world-28')
 def hello():
-    return 'Hello World 27'
+    return 'Hello World 28'
 
 
 server = make_server('', 8000, app)
-print('http://127.0.0.1:8000/api/v1/hello-world-27')
 server.serve_forever()
